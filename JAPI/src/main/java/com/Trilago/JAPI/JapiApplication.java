54a75d35cbc5356ba719ac387d7c209a199f2afb
shinde-omkar-4265 <shindeomkar4265@gmail.com>
2024-06-27 15:44:50 +0530

    JAPI

---
+++ JAPI/src/main/java/com/Trilago/JAPI/JapiApplication.java
@@ -0,0 +1,13 @@
+package com.Trilago.JAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapiApplication.class, args);
+	}
+
+}
---
+++ JAPI/src/test/java/com/Trilago/JAPI/JapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Trilago.JAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
